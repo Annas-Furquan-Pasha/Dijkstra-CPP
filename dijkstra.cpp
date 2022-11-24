bf248489b9f2d817b9718b8d2503ffaecbb5378e
unknown <annasfurquan27@gmail.com>
2022-11-24 14:44:55 +0530

    first commit

---
+++ dijkstra.cpp
@@ -0,0 +1,68 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int mNodes = 0;
+    int SourceNode = 0;
+
+    cout << "Enter number of nodes : ";
+    cin >> mNodes;
+
+    cout << "Enter adjacency matrix :" << endl;
+    int Graph[mNodes][mNodes];
+    for(int i=0; i<mNodes; i++ )
+    {
+        for(int j=0; j<mNodes; j++)
+        {
+            cin >> Graph[i][j];
+        }
+    }
+
+    cout << "Enter source node : " ;
+    cin >> SourceNode;
+
+    int Visited[mNodes] ;  // Array to keep track of visited nodes
+    int Distance[mNodes] ; // Array which has distances to all nodes from source node
+
+    for(int i=0; i< mNodes; i++)   // Initializing visited and distance array
+    {
+        Visited[i] = 0;
+        Distance[i] = 999999;
+    }
+
+    int r = SourceNode;  // Temporary source node
+    Visited[r] = 1;
+    Distance[r] =0;
+    int x = 1;           // Integer which stores number of nodes visited
+
+    while(x != mNodes)
+    {
+        // Updating distances
+        for(int i=0; i<mNodes ; i++)
+        {
+            if(Graph[r][i] !=0 && Visited[i] != 1 && Distance[r]+Graph[r][i] < Distance[i])
+                Distance[i] = Distance[r]+Graph[r][i];
+        }
+        
+        // Finding least distance
+        int leastNo = 99999;
+        r = 0;
+        for(int i=0; i<mNodes; i++)
+        {
+            if(Distance[i] != 0 && Visited[i] != 1 && leastNo > Distance[i] )
+            {
+                leastNo = Distance[i];
+                r = i;
+            }
+        }
+        Visited[r] = 1;
+        x++;
+    }
+
+    cout << "Distance from source node '" << SourceNode << "' to node " << endl;
+    for(int i=0; i<mNodes; i++)
+    {
+        cout << i << " is " << Distance[i] << endl;
+    }
+}
